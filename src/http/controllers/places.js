910d6875fbc1b888a2a1915db69f8d36225e0d44
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-04-04 17:58:17 +0200

    Add json file with region and municipalities

--- src/http/controllers/places.js
+++ src/http/controllers/places.js
@@ -1,12 +1,19 @@
-import places from '../../../data/places';
+import municipalities from '../../../data/municipalities';
 import regions from '../../../data/regions';
+import all from '../../../data/regions+municipalities.json';
 
 module.exports = router => {
   router.get('/places', async (req, res) => {
-    if (req.query.type === 'regions') {
-      await res.json(regions);
-    } else {
-      await res.json(places);
+    switch (req.query.type) {
+      case 'regions':
+        await res.json(regions);
+        break;
+      case 'all':
+        await res.json(all);
+        break;
+      default:
+        await res.json(municipalities);
+        break;
     }
   });
 };
